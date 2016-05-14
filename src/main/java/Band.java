6e1b74d354e5f7f5d8546eded202e9c6f12a7cb6
Elissa Sosland <esosland@gmail.com>
2016-05-14 13:38:38 -0700

    create files, initial setup, add first passing unit test to instantiate bands correctly

---
+++ src/main/java/Band.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.ArrayList;
+import org.sql2o.*;
+
+public class Band {
+  private int id;
+  private String name;
+
+  public Band (String name) {
+    this.name = name;
+  }
+}
---
+++ src/test/java/BandTest.java
@@ -0,0 +1,19 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class BandTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Band_instantiatesCorrectly_true() {
+    Band myBand = new Band("The Roots");
+    assertEquals(true, myBand instanceof Band);
+  }
+
+
+}
